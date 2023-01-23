fc84f50456840155e73385074d4550969912e148
yoonkiwoong <rldnddbs@naver.com>
2023-01-24 02:53:58 +0900

    header생성

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import Article from './pages/Article';
 import Articles from './pages/Articles';
 import Edit from './pages/Edit';
 import Write from './pages/Write';
-
+import Main from './pages/Main';
 const App = ()=>{
   return(
     <div>
       <BrowserRouter>
       <Routes>
+      <Route path='/' element={<Main/>}/>
       <Route path='/articles' element={<Articles/>}/>
       <Route path='/articles/:id' element={<Article/>}/>
       <Route path='/write' element={<Write/>}/>
---
+++ src/pages/Main.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../components/Base/Header'
+
+function Main() {
+  return (
+      <div>
+    <Header/>
+
+    </div>
+  )
+}
+
+export default Main
