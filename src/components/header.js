70e328f429bcd0a5dabdc6fa14f9a7c24235cecd
dddfffddffss <dlhwadlhwa@gmail.com>
2023-01-24 09:51:45 +0900

    map

--- src/components/header.js
+++ src/components/header.js
@@ -5,8 +5,8 @@ const header=()=>{
         <header>
             <a className="logo_name" href='/'>여행후기</a>
             <div className="logo_menubox">
-            <img className="logo_menubox_item logo_menubox_login" src="" alt="profile" href="#"/>
-            <img className="logo_menubox_item logo_menubox_signup" src="comp_res/signup.png" alt="profile" />
+                <img className="logo_menubox_item" src="img/signup.png" alt="profile" />
+                <img className="logo_menubox_item" src="img/login.png" alt="profile" href="#"/>
             </div>
         </header>
     )
